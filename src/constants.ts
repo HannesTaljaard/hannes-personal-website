a2fcc368d581a044678de98d1e7d88555d7e8640
damjanprvc <35609306+damjanprvc@users.noreply.github.com>
2024-02-23 13:52:53 +0100

    basic setup

---
+++ src/constants.ts
@@ -0,0 +1,51 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// export const NAME = 'Hannes Taljaard';
+export const SITE_TITLE = 'Hannes Taljaard | Composer';
+export const SITE_DESCRIPTION = 'Official Website';
+export const HOMEPAGE_URL = 'https://hannestaljaard.com';
+
+// Available navigation or pages for the header navigation
+export const SITE_NAVIGATION = [
+    {
+        name: 'home',
+        href: '/'
+    },
+    {
+        name: 'about',
+        href: '/about'
+    },
+    {
+        name: 'compositions',
+        href: '/compositions'
+    },
+    {
+        name: 'contact',
+        href: '/contact'
+    },
+]
+
+// Browse here: https://icon-sets.iconify.design/
+export const FOOTER_SOCIALS = [
+    {
+        name: 'YouTube',
+        url: 'https://youtube.com/@damjanprvulovic',
+        icon: "mdi:youtube",
+    },
+    {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/damjanprvulovic/',
+        icon: "mdi:instagram",
+    },
+    {
+        name: 'TikTok',
+        url: 'https://www.tiktok.com/@damjanprvulovic',
+        icon: "ic:baseline-tiktok",
+    },
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/damjan-prvulovic-576637153/',
+        icon: "mdi:linkedin",
+    },
+]
